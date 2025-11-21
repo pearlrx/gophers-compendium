432728923060c7bddc24ac49b6bc2e99db6f6005
Pearl <dima_kud2005@mail.ru>
2025-11-22 00:19:41 +0300

    Correct basics file .go

--- basics/hello_world.go
+++ basics/hello_world.go
@@ -1,32 +1,30 @@
-//# Hello, World in Go
-//
-//This is the traditional first program in Go.
-//The goal of this topic is to:
-//
+// Hello, World in Go
+
+// This is the traditional first program in Go.
+// The goal of this topic is to:
+
 //- Understand the minimal structure of a Go program
 //- Learn how `package main` and `func main()` work
 //- See how to print text to the console
 //- Run a Go program using `go run`
 
-//Minimal Go program
+// Minimal Go program
 
-//Breakdown
-//
-//package main
-//Entry point of an executable Go program. Any program that you want to run directly must have package main.
-//
-//import "fmt"
-//Imports the standard library package fmt (formatted I/O). We use it for printing.
-//
-//func main()
-//The main function. Execution starts here.
-//
-//fmt.Println("Hello, Go!")
-//Calls the Println function from the fmt package to print a line to the console.
+// package main
+// Entry point of an executable Go program. Any program that you want to run directly must have package main.
+
+// import "fmt"
+// Imports the standard library package fmt (formatted I/O). We use it for printing.
+
+// func main()
+// The main function. Execution starts here.
+
+// fmt.Println("Hello, Go!")
+// Calls the Println function from the fmt package to print a line to the console.
 
 package main
 
-import "fmt"
+import "fmt" // fmt is a standard package for working with terminal
 
 func main() {
 	fmt.Println("Hello, World!")
